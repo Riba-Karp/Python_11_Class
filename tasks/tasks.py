8e7a60697779b12007b1e351f78687b9f0e77fb2
Veronika <semenova.veronika.a@gmail.com>
2024-12-04 01:45:59 +0300

    Added Task class

---
+++ tasks/tasks.py
@@ -0,0 +1,18 @@
+class Task:
+    def __init__(self, id, title, description, done, priority, due_date):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.done = done
+        self.priority = priority
+        self.due_date = due_date
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "done": self.done,
+            "priority": self.priority,
+            "due_date": self.due_date
+        }
